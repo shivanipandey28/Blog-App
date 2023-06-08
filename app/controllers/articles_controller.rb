41e354ba0194aaf7a06ef500ec405514955b974a
developer <shpandey@bestpeers.com>
2023-06-08 10:22:15 +0530

    edit modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   end
 
     def show
-      byebug
+
       @article = Article.find(params[:id])
     end
 
@@ -24,6 +24,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
   private
     def article_params
       params.require(:article).permit(:title, :body)
